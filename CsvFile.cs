c1c68c0c6dd9fc3ac34cabb1fe2320dbe3064259
sushi01 <sushi01@outlook.jp>
2018-03-12 17:47:46 +0900

    CsvFile sample

---
+++ CsvFile.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+using Microsoft.VisualBasic.FileIO;
+
+
+namespace AlpacaTech
+{
+    /// <summary>
+    /// CSVファイルを２次元配列List<List<string>>に格納するクラス
+    /// </summary>
+    class CsvFile
+    {
+        //  格納されるメンバ
+        public List<List<string>> lines;
+
+        /// <summary>
+        /// コンストラクタ。CSVファイルから読み込み
+        /// </summary>
+        /// <param name="path">ファイルパス</param>
+        /// <param name="delimiters">区切り文字</param>
+        /// <param name="encoding">エンコード指定。Encoding.GetEncoding("shift-jis")等</param>
+        public CsvFile(string path, string delimiters = ",", Encoding encoding = null)
+        {
+            //	指定が無ければUTF-8
+            if (encoding == null)
+            {
+                encoding = Encoding.GetEncoding("utf-8");
+            }
+
+            //	パース開始
+            var parser = new TextFieldParser(path, encoding);
+            using (parser)
+            {
+                //  区切りの指定
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(delimiters);
+
+                // フィールドが引用符で囲まれているか
+                parser.HasFieldsEnclosedInQuotes = true;
+                // フィールドの空白トリム設定
+                parser.TrimWhiteSpace = false;
+
+                //	リスト生成
+                lines = new List<List<string>>();
+
+                // ファイルの終端までループ
+                while (!parser.EndOfData)
+                {
+                    var line = new List<string>();
+                    var row = parser.ReadFields();
+                    foreach (var field in row)
+                    {
+                        line.Add(field);
+                    }
+                    lines.Add(line);
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var csv = new AlpacaTech.CsvFile("data.csv", ",", Encoding.GetEncoding("shift-jis"));
+
+            //  ランダムアクセス([y][x])
+            System.Console.WriteLine(csv.lines[5][3] );
+            System.Console.WriteLine("------------------------");
+
+            //  全てにアクセス
+            foreach (var line in csv.lines)
+            {
+                foreach (var v in line)
+                {
+                    System.Console.WriteLine(v);
+                }
+            }
+
+        }
+    }
+}
